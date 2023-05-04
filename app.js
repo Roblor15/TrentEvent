64fad26c54b63b0a07c9d44a7a7ef4760455648d
Roberto Lorenzon <roberto.lorenzon.2001@gmail.com>
2023-05-04 22:04:23 +0200

    added eslint configuration

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+const express = require('express');
+const logger = require('morgan');
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+const indexRouter = require('./routes/v1/index');
+const usersRouter = require('./routes/v1/users');
 
-let app = express();
+const app = express();
 
 app.use(logger('dev'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
