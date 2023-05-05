aa2308eb21585a4d4bd94b090b6af856984e6de0
Roberto Lorenzon <roberto.lorenzon.2001@gmail.com>
2023-05-05 13:20:07 +0200

    added swagger for api documentation

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const logger = require('morgan');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 const indexRouter = require('./routes/v1/index');
 const usersRouter = require('./routes/v1/users');
+
+const swaggerOptionsV1 = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'TrentEvent',
+            version: '0.0.1',
+        },
+    },
+    apis: ['./routes/v1/*.js'],
+};
+
+const swaggerDocument = swaggerJsDoc(swaggerOptionsV1);
 
 const app = express();
 
@@ -10,6 +25,8 @@ app.use(logger('dev'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/api-docs/v1', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
