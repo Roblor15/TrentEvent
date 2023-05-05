6699016600aad1b45c0cd4a79400a861ecbd3916
Roberto Lorenzon <roberto.lorenzon.2001@gmail.com>
2023-05-05 17:52:36 +0200

    documentation post signup-manager

--- routes/v1/users.js
+++ routes/v1/users.js
@@ -1,9 +1,66 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
-});
+/**
+ * @swagger
+ * /signup-manager:
+ *   post:
+ *     summury: Send a request to register as an Event Manager.
+ *     description: Send a request to register as an Event Manager. I will be notify with an email, if your request is accepted or denied.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               localName:
+ *                 type: string
+ *                 description: The local's name.
+ *                 example: Bar Bello
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: The user's email.
+ *                 example: mario.rossi@gmail.com
+ *               country:
+ *                 type: string
+ *                 description: The country where the local is.
+ *                 example: Italy
+ *               city:
+ *                 type: string
+ *                 description: The city where the local is.
+ *                 example: Trento
+ *               street:
+ *                 type: string
+ *                 description: The street where the local is.
+ *                 example: corso tre novembre
+ *               number:
+ *                 type: integer
+ *                 description: The house number of the local.
+ *                 example: 15
+ *               cap:
+ *                 type: string
+ *                 description: The cap of the city.
+ *                 example: 38122
+ *               localType:
+ *                 type: string
+ *                 description: The type of the local.
+ *                 example: Bar
+ *               localPhotos:
+ *                 type: array
+ *                 minItems: 1
+ *                 maxItems: 5
+ *                 description: Photos of the local.
+ *                 items:
+ *                   type: string
+ *                   format: binary
+ *     responses:
+ *       200:
+ *         desctiption: request succesfully processed
+ *       400:
+ *         desctiption: Malformed request
+ */
+router.post('/signup-manager', function (req, res) {});
 
 module.exports = router;
