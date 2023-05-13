373abeb5a9ae0fd97212dc84ba0fa91e364690b3
Roberto Lorenzon <roberto.lorenzon.2001@gmail.com>
2023-05-13 23:08:11 +0200

    reimplementation notify with google api

--- notify.js
+++ notify.js
@@ -1,21 +1,135 @@
-const nodemailer = require('nodemailer');
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+const { authenticate } = require('@google-cloud/local-auth');
+const { google } = require('googleapis');
+const MailComposer = require('nodemailer/lib/mail-composer');
 
-function sendMail(mailOptions) {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        port: 587,
-        auth: {
-            user: process.env.GMAIL_USER,
-            pass: process.env.GMAIL_PASSWORD,
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/gmail.send'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+    try {
+        const content = await fs.readFile(TOKEN_PATH);
+        const credentials = JSON.parse(content);
+        return google.auth.fromJSON(credentials);
+    } catch (err) {
+        return null;
+    }
+}
+
+/**
+ * Serializes credentials to a file compatible with GoogleAUth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+    const content = await fs.readFile(CREDENTIALS_PATH);
+    const keys = JSON.parse(content);
+    const key = keys.installed || keys.web;
+    const payload = JSON.stringify({
+        type: 'authorized_user',
+        client_id: key.client_id,
+        client_secret: key.client_secret,
+        refresh_token: client.credentials.refresh_token,
+    });
+    await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+    let client = await loadSavedCredentialsIfExist();
+    if (client) {
+        return client;
+    }
+    client = await authenticate({
+        scopes: SCOPES,
+        keyfilePath: CREDENTIALS_PATH,
+    });
+    if (client.credentials) {
+        await saveCredentials(client);
+    }
+    return client;
+}
+
+/**
+ * Lists the labels in the user's account.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+async function listLabels(auth) {
+    const gmail = google.gmail({ version: 'v1', auth });
+    const res = await gmail.users.labels.list({
+        userId: 'me',
+    });
+    const labels = res.data.labels;
+    auth;
+    if (!labels || labels.length === 0) {
+        console.log('No labels found.');
+        return;
+    }
+    console.log('Labels:');
+    labels.forEach((label) => {
+        console.log(`- ${label.name}`);
+    });
+}
+
+function encodeMessage(message) {
+    return Buffer.from(message)
+        .toString('base64')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+}
+
+async function createMail(options) {
+    const mailComposer = new MailComposer(options);
+    const message = await mailComposer.compile().build();
+    return encodeMessage(message);
+}
+
+async function sendMail(options) {
+    const auth = await authorize();
+    const gmail = google.gmail({ version: 'v1', auth });
+
+    const rawMessage = await createMail(options);
+
+    const res = await gmail.users.messages.send({
+        userId: 'me',
+        resource: {
+            raw: rawMessage,
         },
     });
 
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log('error');
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
+    return res.data.id;
 }
+
+//   sendMail({
+//       to: 'roberto.lorenzon.2001@gmail.com',
+//       subject: 'Hello Guy 🚀',
+//       text: 'This email is sent from the command line',
+//       // html: `<p>🙋🏻‍♀️  &mdash; This is a <b>test email</b> from <a href="https://digitalinspiration.com">Digital Inspiration</a>.</p>`,
+//       textEncoding: 'base64',
+//       headers: [
+//           { key: 'X-Application-Developer', value: 'Amit Agarwal' },
+//           { key: 'X-Application-Version', value: 'v1.0.0.2' },
+//       ],
+//   })
+//       .then(console.log)
+//       .catch(console.log);
+
+module.exports = sendMail;
